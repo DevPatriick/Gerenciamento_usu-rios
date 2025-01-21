52e15b0aa13f51b8794203c43591888823c29e8b
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-01-20 23:58:05 -0300

    Ao inves de ter uma variavel para cada sexo, inputGenderM e inputGenderF, criamos uma váriavel let gender e buscamos apenas o radio marcado usando o querySelectorAll para puxar todos os elementos com o nome gender, porém para eu saber qual está marcado usei :checked para saber qual foi selecionado, além disso colocamos um id no elemento do admin para poder busca-lo no arquivo index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// Variaveis
+
 let inputName = document.querySelector("#exampleInputName");
-let inputGenderM= document.querySelector("#exampleInputGenderM");
-let inputGenderF = document.querySelector("#exampleInputGenderF");
+
+// Ao inves de ter duas váriaveis eu posso usar o [name=gender] para ver as está 
+// marcada para não ter duas váriaveis e ao usar o [name=gender]:checked eu estou querendo buscar apenas os
+// selecionados
+let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+// let inputGenderM= document.querySelector("#exampleInputGenderM");
+// let inputGenderF = document.querySelector("#exampleInputGenderF");
+console.log(gender)
+
 let inputBirth = document.querySelector("#exampleInputBirth");
 let inputCountry = document.querySelector("#exampleInputCountry");
 let inputEmail = document.querySelector("#exampleInputEmail1");
 let inputPassword = document.querySelector("#exampleInputPassword1");
 let inputFile = document.querySelector("#exampleInputFile");
-let admin = document.querySelector("#")
+let admin = document.querySelector("#inputAdmin");
 let buttonSaveForms = document.querySelector(".btn");
