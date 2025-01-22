c140586cda93a7d79c5981c6e500680a971253b4
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-01-21 23:41:13 -0300

    AULA 37 TRABALHANDO COM FUNÇÕES - Criei um função chamada addLine para adicionar um usuário, essa função recebe um parametro que chamei de dataUser, dentro da função criei um const para criar o elemento tr, esta variavel chamada de trUser eu vou criar meu elemento HTML dinamicamente usando o templateStrings, para dizer para o js onde incluir este elemento que estou criando busco um id chamado tableUser e digo que o elemento que criei vai ser seu filho, assim incluindo dentro do elemento do id. Executo a função na função do addEventListener passando o parametro do usuário como a função addLine pede.

--- index.js
+++ index.js
@@ -27,23 +27,45 @@ let fields = document.querySelectorAll("#form-user-create [name]");
 
 const user = {}
 
-document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+function addLine(dataUser) {
+
+    const trUser = document.createElement("tr")
+    trUser.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>`;
+
+    document.getElementById("tableUser").appendChild(trUser)
+}
+
+
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
 
     event.preventDefault();
 
-    fields.forEach(function(field, index){
-
-        if(field.name == "gender"){
-            if(field.checked === true ){ // ou apenas field.checked
+    fields.forEach(function (field, index) {
+        if (field.name == "gender") {
+            if (field.checked === true) { // ou apenas field.checked
                 user[field.name] = field.name;
             }
-            
-        } else{
-            
+
+        } else {
+
             user[field.name] = field.value;
-    
+
         }
-    
     });
+
+    addLine(user);
 
 })
