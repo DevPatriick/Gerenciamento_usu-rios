997b42335e8f3d7918333e0e006901a5c58d71ac
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-01-23 22:46:40 -0300

    AULA 41 - TRABALHANDO COM ARQUIVOS - FILE READER - Usamos o File Reader para ler o caminho da minha imagem para mostrarmos ela na tela para o usuário, criei um novo metodo getPhoto passando uma funçao como retorno, no getPhoto eu pego o campo photo filtrando pelo array usando o filter e pegando a posição filtrada, depois passo um onload para que ele me passe o resultado do carregamento e depois leio o arquivo no readAsDataURL passando o file como paramentro, depois executo a função getPhoto passando a imagem para o value.photo.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,7 @@ class UserController {
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
-        
+
         // chamando o metodo ao instanciar o UserController
         this.submit()
 
@@ -16,16 +16,51 @@ class UserController {
     // evento de submit
     // ao clicar no button ele não vai carregar a página por causa do preventDefault()
     // e vai executar o metodo addLine que por parametro recebe o metodo getValue())
-    submit(){
+    submit() {
 
         this.formEl.addEventListener("submit", (event) => {
             event.preventDefault();
-            this.addLine(this.getValue());
-        
+            let value = this.getValue();
+
+            // aqui eu executo a função getPhoto chamando a função de callback, passo parametro da minha
+            // função que é o content que é a minha imagem
+            // value.photo recebe o content e depois executo o addLine para incluir no HTML
+
+            this.getPhoto((content) => {
+                value.photo = content;
+                this.addLine(value);
+            })
+
         })
     }
 
-     
+    // no fileReard estou instanciando o new FileReader(), logo depois eu coloco o atributo elements e nele 
+    // filtro os elementos do meu Array para pegar apenas o campo com name photo
+    // depois no atributo let file digo que ele recebe o elemento filtrado acima na posição 0 e o primeiro arquivo
+    // do elemento
+    // agora dentro do fileReard eu chamo o metodo onload para ele carregar a imagem para mim
+    // e me retorna se ele foi concluído 
+    // depois o fileReard.readAsDataURL ele lê o meu file e quando ele finaliza a leitura passa o callback
+
+    getPhoto(callback) {
+        let fileReard = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+            if (item.name === "photo") {
+                return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReard.onload = () => {
+            callback(fileReard.result);
+        }
+
+        fileReard.readAsDataURL(file)
+    }
+
+
     // Aqui eu estou percorrendo o meu formulário pelo formEl e validando qual gender está marcado
     // Ao percorrer estou retornando neste metodo os valores que estão dentro dos campos do formulário.
     getValue() {
@@ -58,13 +93,13 @@ class UserController {
 
     }
 
-     // Neste metodo eu recebo o parametro dataUser que seria as informações preenchidas no formulário 
-     // e crio o elemento com o template string
+    // Neste metodo eu recebo o parametro dataUser que seria as informações preenchidas no formulário 
+    // e crio o elemento com o template string
     addLine(dataUser, tableId) {
 
         this.tableEl.innerHTML = `
         <tr>
-            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
@@ -74,7 +109,7 @@ class UserController {
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>`;
-    
+
     }
 
 }
