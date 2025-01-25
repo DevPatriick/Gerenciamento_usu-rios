2e51e7040aea5019959b16b2cd15e748ccebdfe4
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-01-24 22:16:55 -0300

    AULA 54 - EXCLUINDO DADOS DO OBJETO - Para excluir um user eu busquei o tr que foi selecionado pelo btn de delete, ao clicar faço um if para confirmar se o usuário ainda quer excluir o usuários se ele confirmar tr.remove() e this.uptadeCount() para atulizar o numero de users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -39,7 +39,7 @@ class UserController {
 
 
             this.uptadeCount();
-           
+
 
             this.getPhoto(this.formElPut).then(
                 (content) => {
@@ -227,7 +227,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -240,6 +240,15 @@ class UserController {
     }
 
     addEventsTr(tr) {
+        tr.querySelector(".btn-delete").addEventListener('click', e => {
+            if (confirm("Deseja realmente excluir?")) {
+                tr.remove();
+                this.uptadeCount()
+            }
+        });
+
+
+
         tr.querySelector(".btn-edit").addEventListener('click', e => {
             let json = JSON.parse(tr.dataset.user);
             let form = document.querySelector("#form-user-uptade");
